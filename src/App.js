d3371408612b736f14cff571e2da63179e35a107
Apuroopreddyg <150883132+Apuroopreddyg@users.noreply.github.com>
2023-12-19 10:52:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ function App() {
   return (
     <>
       <Router>
+        <nav style={navStyle}>
+            <Link to="/Home" style={linkStyle}>Home<Link>
+            <Link to="/" style={linkStyle}>Login<Link>
+            <Link to="/Register" style={linkStyle}>Register<Link>
+        </nav>
         <Toaster position='top-center' toastOptions={{duration:2000}}></Toaster>
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
@@ -25,5 +30,24 @@ function App() {
     </>
   );
 }
+const navStyle = {
+  background: '#333',
+  color: '#fff',
+  display: 'flex',
+  justifyContent: 'space-around',
+  padding: '10px',
+};
+
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none',
+  padding: '8px 20px',
+  border: 'none',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  textAlign: 'center',
+  transition: 'background-color 0.3s ease',
+  textTransform: 'uppercase',
+};
 
 export default App;
