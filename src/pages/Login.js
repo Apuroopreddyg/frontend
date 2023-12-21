09363558cd7bd6c666fb91ea9776d429fc22f254
Apuroopreddyg <150883132+Apuroopreddyg@users.noreply.github.com>
2023-12-21 11:50:51 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './login.css'
+// import './login.css'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 import {toast} from 'react-hot-toast'
